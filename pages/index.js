341354ff3895b8a147cde8b4ed369a8544e0fe6c
Joaquin Cortes <joaquin.cortes@gat-blac.com>
2022-08-17 17:34:26 -0400

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Client Register APP</title>
+        <meta name='description' content="Client Register APP" />
+      </Head>
+      <h1>Hola mundo</h1>
+    </>
+  )
+}
